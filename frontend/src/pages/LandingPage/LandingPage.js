c949c01f43f0d8839316d0338f6864f10beee18b
DreamofHeaven <eunjeong0223@naver.com>
2023-08-08 22:57:19 +0900

    [ADD] 프론트엔드 로그인 화면 추가

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -4,10 +4,21 @@ import Footer from '../../components/Footer'
 import LoginForm from '../../components/LoginForm'
 
 function LandingPage() { 
+  const [showLoginForm, setShowLoginForm] = useState(false);
+
+  const toggleLoginForm = () => { 
+    setShowLoginForm(!showLoginForm);
+  };
+
   return (
     <main className='page-wrap'>
       <div className='page-main'>  
-        <LoginForm />
+        <h1>내 안의 긍정이</h1>
+        {showLoginForm ? <LoginForm /> : 
+          <button className='login-btn' onClick={toggleLoginForm}>
+            <p>로그인</p>
+          </button>
+        }
       </div>
       <Footer/>
     </main>
